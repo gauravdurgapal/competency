cf673888a1111be667c736c23fcbd95e0daec375
gaurav421 <gauravdurgapal333@gmail.com>
2021-07-11 20:41:38 +0530

    added update functionality, enable logger, handle duplicate entries

--- src/main/java/com/capgemini/competency/EmployeeController.java
+++ src/main/java/com/capgemini/competency/EmployeeController.java
@@ -2,7 +2,7 @@ package com.capgemini.competency;
 
 import java.util.List;
 
-
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +10,14 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.competency.model.EmployeeDetails;
+import com.capgemini.competency.model.Response;
 import com.capgemini.competency.service.EmployeeService;
+import ch.qos.logback.classic.Logger;
 
 @RestController
 public class EmployeeController {
@@ -23,35 +25,57 @@ public class EmployeeController {
 	@Autowired
 	EmployeeService empService;
 	
-	@PostMapping("/insert")
-	public void addDetail(@RequestBody EmployeeDetails empDetail) {
-		empService.save(empDetail);
-
+	private Logger logger= (Logger) LoggerFactory.getLogger(EmployeeController.class);
+	
+	
+	//create post mapping that create the single employee detail in the database
+	@PostMapping("/addEmployee")
+	public Response addDetail(@RequestBody EmployeeDetails empDetail) {
+		Response response= new Response();
+		logger.info("Received Create Request for single Employee");
+		response=empService.save(empDetail);
+		logger.info("Create Request Executed for single Employee");
+		return response;
 	}
 	
-	@PostMapping("/addmultiple")
+	//create post mapping that create the multiple employee details in the database 
+	@PostMapping("/addmultipleEmployee")
 	public ResponseEntity<EmployeeDetails> addMultiple(@RequestBody List<EmployeeDetails> empAll) {
-		
+		logger.info("Received Create Request for multiple Employees");
 		empService.saveAll(empAll);
+		logger.info("Create Request Executed for multiple Employees");
 		return new ResponseEntity<EmployeeDetails>(HttpStatus.OK);
 	}
 	
-	@DeleteMapping("/delete")
-	public void delete(@RequestParam int Id) {
-		empService.delete(Id);
+	//create a delete mapping that deletes a specified employee 
+	@DeleteMapping("/delete/{id}")
+	public void delete(@PathVariable int id) {
+		logger.info("Received Delete Request");
+		empService.delete(id);
+		logger.info("Delete Request executed");
 	}
 	
-	@GetMapping("/getMappings")
+	//create a get mapping that retrieves all the employee detail from the database
+	@GetMapping("/getAllEmployeeDetails")
 	public List<EmployeeDetails> getEmployeeDetails(){
+		logger.info("Received retreive Request for all employees");
 		return empService.getEmployeeDetails();
 	}
 	
-	@GetMapping("/{globalId}")
-    public EmployeeDetails findEmployeebyId(@PathVariable("globalId") int globalId)
+	//create a get mapping that retrieves the detail of a specific employee
+	@GetMapping("/getEmployeeDetail/{globalId}")
+   // public EmployeeDetails findEmployeebyId(@PathVariable("globalId") int globalId)
+	public EmployeeDetails findEmployeebyId(@PathVariable int globalId)
     {
-       
-        return empService.findById(globalId);
-       
+		logger.info("Received retreive Request for specific employee");
+        return empService.findById(globalId);  
     }
 
+	//create a update mapping that update the detail of a specific employee
+	@PutMapping("/updateEmployee")
+	 public void updateEmployeeDetails(@RequestBody EmployeeDetails empDetail)
+	    {
+			logger.info("Received update Request for specific employee");
+	        empService.update(empDetail);  
+	    }
 }
--- src/main/java/com/capgemini/competency/service/EmployeeService.java
+++ src/main/java/com/capgemini/competency/service/EmployeeService.java
@@ -3,12 +3,14 @@ package com.capgemini.competency.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.capgemini.competency.model.EmployeeDetails;
+import com.capgemini.competency.model.Response;
 import com.capgemini.competency.model.User;
 
 @Service
@@ -22,8 +24,22 @@ public class EmployeeService {
 	}
 	*/
 	
-	public void save(EmployeeDetails empDetails) {
+	public Response save(EmployeeDetails empDetails) {
+		
+		EmployeeDetails e2=empInt.findById(empDetails.getGlobalId());
+		Response response =new Response();
+		if(e2!=null)
+		{
+		    response.setMessage("Entry already exists in the database");
+		    response.setStatus("409");  
+		}
+		else
+		{
 		 empInt.save(empDetails);
+		 response.setMessage("Entry successfully added in the database");
+		 response.setStatus("200"); 
+		}
+		 return response;
 	}
 	
 	public void saveAll(List<EmployeeDetails> empList) {
@@ -32,7 +48,7 @@ public class EmployeeService {
 	}
 	
     public void delete(Integer Id) {
-	empInt.deleteById(Id);
+	     empInt.deleteById(Id);
 	} 
     
     public List<EmployeeDetails> getEmployeeDetails(){
@@ -40,6 +56,24 @@ public class EmployeeService {
 	}
 
     public EmployeeDetails findById(int globalId) {
-        return empInt.findById(globalId);
+
+     return(empInt.findById(globalId));
     }
+
+	public void update(EmployeeDetails empDetail) {
+		EmployeeDetails e2=empInt.findById(empDetail.getGlobalId());
+		e2.setEmpName(empDetail.getEmpName());
+		e2.setGender(empDetail.getGender());
+		e2.setJobTitle(empDetail.getJobTitle());
+		e2.setEmpSubGroup(empDetail.getEmpSubGroup());
+		e2.setTimesheetComplaince(empDetail.getTimesheetComplaince());
+		e2.setSupervisorName(empDetail.getSupervisorName());
+		e2.setClientTeam(empDetail.getClientTeam());
+		e2.setEmailId(empDetail.getEmailId());
+		e2.setBaseLocation(empDetail.getBaseLocation());
+		e2.setPersonalArea(empDetail.getPersonalArea());
+		e2.setLocation(empDetail.getLocation());
+		e2.setLocalId(empDetail.getLocalId());
+		empInt.save(e2);
+	}
 }
